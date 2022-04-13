372aca4aae0050f6b7530247d24fabf5e7ded27d
gaetanmuck <gaetan.muck@gmail.com>
2022-04-13 17:29:05 +0200

    react-components

---
+++ react-components/src/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'web-components';
+
+import { defineCustomElements } from 'web-components/loader';
+
+defineCustomElements();
+export const GvButton = /*@__PURE__*/createReactComponent<JSX.GvButton, HTMLGvButtonElement>('gv-button');
+export const GvPerson = /*@__PURE__*/createReactComponent<JSX.GvPerson, HTMLGvPersonElement>('gv-person');
