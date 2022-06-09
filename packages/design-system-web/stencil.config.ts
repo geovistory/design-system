0050efb8b089de76c4550a41a7e46350da35ab78
joschne <jonas.schneider@balcab.ch>
2022-06-09 12:00:57 +0200

    fix stencil config

--- packages/design-system-web/stencil.config.ts
+++ packages/design-system-web/stencil.config.ts
@@ -21,12 +21,12 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
     angularOutputTarget({
-      componentCorePackage: 'design-system-web',
+      componentCorePackage: '@geovistory/design-system-web',
       directivesProxyFile: '../design-system-angular/projects/component-library/src/lib/stencil-generated/components.ts',
       directivesArrayFile: '../design-system-angular/projects/component-library/src/lib/stencil-generated/index.ts',
     }),
     react({
-      componentCorePackage: 'design-system-web',
+      componentCorePackage: '@geovistory/design-system-web',
       proxiesFile: '../design-system-react/src/components/stencil-generated/index.ts',
       includeDefineCustomElements: true,
     }),
