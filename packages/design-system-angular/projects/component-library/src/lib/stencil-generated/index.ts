f8c0e86932dd85d05acb87caab6a8e5ec923680a
gaetanmuck <gaetan.muck@gmail.com>
2022-06-09 10:56:01 +0200

    partial commit - test workflow

--- packages/design-system-angular/projects/component-library/src/lib/stencil-generated/index.ts
+++ packages/design-system-angular/projects/component-library/src/lib/stencil-generated/index.ts
@@ -2,17 +2,5 @@
 import * as d from './components';
 
 export const DIRECTIVES = [
-  d.GeovButton,
-  d.GeovCard,
-  d.GeovColumn,
-  d.GeovCompanyCard,
-  d.GeovDivider,
-  d.GeovHeading,
-  d.GeovIcon,
-  d.GeovLink,
-  d.GeovLogo,
-  d.GeovNavbar,
-  d.GeovProjectCard,
-  d.GeovRow,
-  d.GeovText
+  
 ];
