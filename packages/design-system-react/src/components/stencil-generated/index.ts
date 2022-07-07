209d6cf41c4f2090c9b10f28def0570aae7eaeed
joschne <jonas.schneider@balcab.ch>
2022-07-07 15:23:50 +0000

    chore(): update build artifacts

--- packages/design-system-react/src/components/stencil-generated/index.ts
+++ packages/design-system-react/src/components/stencil-generated/index.ts
@@ -5,8 +5,9 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from '@geovistory/design-system-web';
 
+import { defineCustomElements } from '@geovistory/design-system-web/loader';
 
-
+defineCustomElements();
 export const IonAccordion = /*@__PURE__*/createReactComponent<JSX.IonAccordion, HTMLIonAccordionElement>('ion-accordion');
 export const IonAccordionGroup = /*@__PURE__*/createReactComponent<JSX.IonAccordionGroup, HTMLIonAccordionGroupElement>('ion-accordion-group');
 export const IonActionSheet = /*@__PURE__*/createReactComponent<JSX.IonActionSheet, HTMLIonActionSheetElement>('ion-action-sheet');
