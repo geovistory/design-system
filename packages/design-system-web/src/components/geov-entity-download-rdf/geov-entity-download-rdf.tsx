2d595236478900cb9c911b5e595c5773d7a79e8b
joschne <jonas.schneider@balcab.ch>
2022-12-08 16:48:51 +0100

    add modal

--- packages/design-system-web/src/components/geov-entity-download-rdf/geov-entity-download-rdf.tsx
+++ packages/design-system-web/src/components/geov-entity-download-rdf/geov-entity-download-rdf.tsx
@@ -1,4 +1,5 @@
 import { Component, Host, h, Prop } from '@stencil/core';
+import { downloadOutline } from 'ionicons/icons';
 
 @Component({
   tag: 'geov-entity-download-rdf',
@@ -27,18 +28,34 @@ export class GeovEntityDownloadRdf {
    */
   @Prop() fill: 'clear' | 'outline' | 'solid' | 'default';
 
+  modal: HTMLIonModalElement;
+
   render() {
     return (
       <Host>
-        <slot>
-          <ion-button id="click-trigger">
-            Download RDF <ion-icon name="download-outline"></ion-icon>
-          </ion-button>
-          <ion-popover trigger="click-trigger" trigger-action="hover">
-            <ion-content class="ion-padding">Hello World!</ion-content>
-          </ion-popover>
-        </slot>
+        <ion-modal id="example-modal" trigger="open-custom-dialog" ref={element => (this.modal = element)}>
+          <ion-header>
+            <ion-toolbar>
+              <ion-buttons slot="start">
+                <ion-button onClick={() => this.dismiss()}>Cancel</ion-button>
+              </ion-buttons>
+              <ion-title>Download RDF</ion-title>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content class="ion-padding">
+            <ion-item>
+              <ion-label>RDF XML</ion-label>
+              <ion-button slot="end">
+                <ion-icon slot="icon-only" icon={downloadOutline} size="small"></ion-icon>
+              </ion-button>
+            </ion-item>
+          </ion-content>
+        </ion-modal>
+        <slot></slot>
       </Host>
     );
+  }
+  dismiss() {
+    this.modal.dismiss();
   }
 }
