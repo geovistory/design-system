e4877c89c273709f1e265b4e32db08a4d91ceafd
joschne <jonas.schneider@balcab.ch>
2022-12-12 17:06:11 +0100

    improve uri label display

--- packages/design-system-web/.storybook/config/defaulSparqlEndpoint.tsx
+++ packages/design-system-web/.storybook/config/defaulSparqlEndpoint.tsx
@@ -1,1 +1,2 @@
-export const DEFAULT_SPARQL_ENDPOINT = 'https://sparql-dev.geovistory.org/api_v1_community_data';
+export const DEFAULT_SPARQL_ENDPOINT = 'https://sparql.geovistory.org/api_v1_community_data';
+export const AMPI_SPARQL_ENDPOINT = 'https://sparql.geovistory.org/api_v1_project_924033';
