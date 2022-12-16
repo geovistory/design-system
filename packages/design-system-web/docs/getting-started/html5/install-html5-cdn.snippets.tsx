b5ff073c5919cbcd1880aa3c958dd77d87a647f8
joschne <jonas.schneider@balcab.ch>
2022-12-16 13:41:54 +0100

    fix in docs

--- packages/design-system-web/docs/getting-started/html5/install-html5-cdn.snippets.tsx
+++ packages/design-system-web/docs/getting-started/html5/install-html5-cdn.snippets.tsx
@@ -1,6 +1,6 @@
 import { version } from '../../../package.json';
 export const html5Cdn = `<!DOCTYPE html>
-<html lang="en">
+<html lang="en" mode="ios">
 
 <head>
     <meta charset="UTF-8">
@@ -21,7 +21,7 @@ export const html5Cdn = `<!DOCTYPE html>
 
 </html>`;
 export const html5NodeModules = `<!DOCTYPE html>
-<html lang="en">
+<html lang="en" mode="ios">
 
 <head>
     <meta charset="UTF-8">
@@ -29,11 +29,11 @@ export const html5NodeModules = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@geovistory/design-system-web@${version}/dist/design-system-web/design-system-web.css" />
+        href="node_modules/@geovistory/design-system-web/dist/design-system-web/design-system-web.css" />
     <script type="module"
-        src="https://cdn.jsdelivr.net/npm/@geovistory/design-system-web@${version}/dist/design-system-web/design-system-web.esm.js"></script>
+        src="node_modules/@geovistory/design-system-web/dist/design-system-web/design-system-web.esm.js"></script>
     <script nomodule
-        src="https://cdn.jsdelivr.net/npm/@geovistory/design-system-web@${version}/dist/design-system-web/design-system-web.js"></script>
+        src="node_modules/@geovistory/design-system-web/dist/design-system-web/design-system-web.js"></script>
 </head>
 
 <body>
