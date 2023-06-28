152d2f6f4b03acdd5e101270193cd7bc2184148c
joschne <jonas.schneider@balcab.ch>
2023-06-28 10:29:35 +0200

    refactor dev mode

--- packages/design-system-web/stencil.dev.config.ts
+++ packages/design-system-web/stencil.dev.config.ts
@@ -1,30 +1,12 @@
 import { Config } from '@stencil/core';
-import { angularGenerator } from './.build/stencil.bindings.angular';
 import { stencilBasicConfig } from './.build/stencil/stencil.basic.config';
-import { reactGenerator } from './.build/stencil.bindings.react';
 
 export const config: Config = {
   ...stencilBasicConfig,
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
-    angularGenerator(),
-    reactGenerator({
-      componentCorePackage: '../../dist/types',
-      loaderDir: '../../loader',
-      proxiesFile: './.storybook/stencil-generated/component.ts',
-    }),
   ],
 };
