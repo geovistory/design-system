83d776020b98532e01800258970a89262c40c1bb
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-07-17 16:08:01 +0200

    update storybook

--- packages/design-system-web/.storybook/main.js
+++ packages/design-system-web/.storybook/main.js
@@ -2,11 +2,8 @@
 module.exports = {
   // "stories": ["../src/components/geov-carousel/geov-carousel.stories.tsx",
   // "../src/components/geov-entity-download-rdf/geov-entity-download-rdf.stories.tsx", "../docs/welcome.stories.tsx"],
- "stories": ["../docs/**/*.stories.mdx", "../docs/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ["../docs/**/*.stories.mdx", "../docs/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  core: {
-    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
-  },
   "framework": {
     name: "@storybook/web-components-vite",
     options: {}
