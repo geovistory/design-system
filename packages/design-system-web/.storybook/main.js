10b3baa349e97ea6afe2be4af4cb3f8397ae40eb
joschne <jonas.schneider@balcab.ch>
2023-07-19 11:49:25 +0200

    refactor main.js

--- packages/design-system-web/.storybook/main.js
+++ packages/design-system-web/.storybook/main.js
@@ -1,18 +1,16 @@
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 module.exports = {
-  // "stories": ["../src/components/geov-carousel/geov-carousel.stories.tsx",
-  // "../src/components/geov-entity-download-rdf/geov-entity-download-rdf.stories.tsx", "../docs/welcome.stories.tsx"],
- "stories": ["../docs/**/*.stories.mdx", "../docs/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  stories: ['../docs/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   core: {
     builder: '@storybook/builder-vite', // 👈 The builder enabled here.
   },
-  "framework": {
-    name: "@storybook/web-components-vite",
-    options: {}
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {},
   },
-  "staticDirs": ["../dist/design-system-web"],
+  staticDirs: ['../dist/design-system-web'],
   docs: {
-    autodocs: 'tag'
-  }
+    autodocs: 'tag',
+  },
 };
