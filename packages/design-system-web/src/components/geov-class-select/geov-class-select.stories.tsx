8b15f92372a70ad7fab18b14db7694d3e6e69ccd
joschne <jonas.schneider@balcab.ch>
2023-07-19 14:18:35 +0200

    extract items array and improve description

--- packages/design-system-web/src/components/geov-class-select/geov-class-select.stories.tsx
+++ packages/design-system-web/src/components/geov-class-select/geov-class-select.stories.tsx
@@ -5,7 +5,7 @@ import componentApi from './docs-component-api.md?raw';
 import overview from './docs-overview.md?raw';
 export default {
   title: 'Data Components/Explorer/Filters/Class Select',
-    tags: ['autodocs'],
+  tags: ['autodocs'],
   parameters: {
     viewMode: 'docs',
     docs: {
@@ -13,103 +13,57 @@ export default {
     },
   },
 };
+const items = [
+  {
+    classLabel: 'Class 1',
+    classUri: 'uri1',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 2',
+    classUri: 'uri2',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 3',
+    classUri: 'uri3',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 4',
+    classUri: 'uri4',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 5',
+    classUri: 'uri5',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 6',
+    classUri: 'uri6',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 7',
+    classUri: 'uri7',
+    instanceCount: 98443,
+  },
+  {
+    classLabel: 'Class 8',
+    classUri: 'uri8',
+    instanceCount: 98443,
+  },
+];
+/**
+ * Open console to see selected items emitted by onSelectionChanged().
+ */
 export const ClassSelectCheckedOnInit = stencilWrapper(
-  <geov-class-select
-    checkedOnInit={true}
-    items={[
-      {
-        classLabel: 'Class 1',
-        classUri: 'uri1',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 2',
-        classUri: 'uri2',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 3',
-        classUri: 'uri3',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 4',
-        classUri: 'uri4',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 5',
-        classUri: 'uri5',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 6',
-        classUri: 'uri6',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 7',
-        classUri: 'uri7',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 8',
-        classUri: 'uri8',
-        instanceCount: 98443,
-      },
-    ]}
-    onSelectionChanged={e => {
-      console.log(e.detail.selectedClassUris);
-    }}
-  ></geov-class-select>,
+  <geov-class-select checkedOnInit={true} items={items} onSelectionChanged={e => console.log(e.detail.selectedClassUris)}></geov-class-select>,
 );
+/**
+ * Open console to see selected items emitted by onSelectionChanged().
+ */
 export const ClassSelectUncheckedOnInit = stencilWrapper(
-  <geov-class-select
-    checkedOnInit={false}
-    items={[
-      {
-        classLabel: 'Class 1',
-        classUri: 'uri1',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 2',
-        classUri: 'uri2',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 3',
-        classUri: 'uri3',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 4',
-        classUri: 'uri4',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 5',
-        classUri: 'uri5',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 6',
-        classUri: 'uri6',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 7',
-        classUri: 'uri7',
-        instanceCount: 98443,
-      },
-      {
-        classLabel: 'Class 8',
-        classUri: 'uri8',
-        instanceCount: 98443,
-      },
-    ]}
-    onSelectionChanged={e => {
-      console.log(e.detail.selectedClassUris);
-    }}
-  ></geov-class-select>,
+  <geov-class-select checkedOnInit={false} items={items} onSelectionChanged={e => console.log(e.detail.selectedClassUris)}></geov-class-select>,
 );
