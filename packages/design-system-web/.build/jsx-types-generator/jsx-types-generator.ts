82a811d1d5c9381de131f33fbceed340d01c1a6c
joschne <jonas.schneider@balcab.ch>
2023-07-20 15:44:00 +0200

    fix build by improving jsx txpes generator

--- packages/design-system-web/.build/jsx-types-generator/jsx-types-generator.ts
+++ packages/design-system-web/.build/jsx-types-generator/jsx-types-generator.ts
@@ -1,47 +1,37 @@
 import { OutputTargetCustom } from '@stencil/core/internal';
-import { readFileSync, writeFileSync } from 'fs';
-import path from 'path';
+
+import { cleanupImports, createJsxForReact, decompose, mergeDecomposed, readFile, recompose, writeFile } from './lib';
 export const jsxTypesGenerator: OutputTargetCustom = {
   type: 'custom',
   name: 'jsx-types',
   generator: async () => {
-    // read ionic components file
-    const sourcePath = path.join(__dirname, '../../node_modules/@ionic/core/dist/types/components.d.ts');
-    const sourceFile = readFileSync(sourcePath).toString();
-    // adapt imports
-    const targetFile = sourceFile.replace(/from ".*/gm, 'from "@ionic/core/dist";');
-    // write components.d.ts
-    const targetPath = path.join(__dirname, '../../src/components-ionic.d.ts');
-    writeFileSync(targetPath, targetFile);
-    createJsxForReact('components-ionic.d.ts', 'components-ionic-react.d.ts');
+    // read and decompose ionicon type file
+    const ioniconsTypesRaw = readFile('../../node_modules/ionicons/dist/types/components.d.ts');
+    const ioniconsTypes = ioniconsTypesRaw.replace(/from ".*/gm, 'from "@ionic/core";');
+    const ioniconsDecomposed = decompose(ioniconsTypes);
+
+    // read and decompose ionic core type file
+    const ionicTypesRaw = readFile('../../node_modules/@ionic/core/dist/types/components.d.ts');
+    const ionicTypes = ionicTypesRaw.replace(/from ".*/gm, 'from "@ionic/core";');
+    const ionicDecomposed = decompose(ionicTypes);
+
+    // read and decompose geov type file
+    const geovTypes = readFile('../../src/components.d.ts');
+    const geovDecomposed = decompose(geovTypes);
+
+    // merge decomposed
+    const merged = mergeDecomposed([ioniconsDecomposed, ionicDecomposed, geovDecomposed]);
+
+    // cleanup imports
+    merged.import = cleanupImports(merged.import);
+
+    // recompose all types into one file
+    const recomposed = recompose(merged);
+
+    // write the file
+    writeFile('../../src/components-all.d.ts', recomposed);
+
     createJsxForReact('components.d.ts', 'components-react.d.ts');
   },
 };
 
-function createJsxForReact(sourceFileName, targetFileName) {
-  const sourcePath = path.join(__dirname, '../../src', sourceFileName);
-  const sourceFile = readFileSync(sourcePath).toString();
-
-
-  // this typings are not perfect, but it serves as a hack.
-  const targetFile = sourceFile
-    .replace(
-      'declare module "@stencil/core"',
-      `
-    export interface StyleReactProps {
-      class?: string;
-      style?: { [key: string]: any };
-    }
-    export type StencilReactExternalProps<PropType, ElementType> = PropType &
-      Omit<React.HTMLAttributes<ElementType>, 'style', 'class'> &
-      StyleReactProps;
-
-    declare global`,
-    )
-    .replace(/LocalJSX\.(\w+)\b/g, 'StencilReactExternalProps<LocalJSX.$1, JSXBase.HTMLAttributes<HTML$1Element>>')
-    // .replace(/LocalJSX\.(\w+)\b/g, 'React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & LocalJSX.$1, HTML$1Element>')
-    .replace(/\s& JSXBase.HTMLAttributes<.*/g, ';');
-
-  const  targetPath = path.join(__dirname, '../../src', targetFileName);
-  writeFileSync(targetPath, targetFile);
-}
