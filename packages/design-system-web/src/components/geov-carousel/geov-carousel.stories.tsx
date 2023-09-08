9a611d75e0aec7701ce68a1d4f4c027b3008026f
Alexandre P <perraud.alexandre@gmail.com>
2023-09-08 11:56:35 +0200

    Images are back

--- packages/design-system-web/src/components/geov-carousel/geov-carousel.stories.tsx
+++ packages/design-system-web/src/components/geov-carousel/geov-carousel.stories.tsx
@@ -3,6 +3,8 @@ import { docsTemlpate } from '../../../.storybook/templates/docsTemplate';
 import { stencilWrapper } from '../../../.storybook/lib/stencilWrapper';
 import componentApi from './docs-component-api.md?raw';
 import overview from './docs-overview.md?raw';
+import { image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12 } from './images';
+
 export default {
   title: 'Design Components/Carousel',
   tags: ['autodocs'],
@@ -17,19 +19,6 @@ export default {
 export const Carousel = await stencilWrapper(
   <geov-carousel
     style={{ height: '400px', maxWidth: '600px' }}
-    images={[
-      'https://www.geovistory.org/carousel-1_maquette-lugdunum.jpg',
-      'https://www.geovistory.org/carousel-2_tapisserie-bayeux.jpg',
-      'https://www.geovistory.org/carousel-3_vitrail.jpg',
-      'https://www.geovistory.org/carousel-4_astrolabe-arabe.jpg',
-      'https://www.geovistory.org/carousel-5_quetzalcoatl.jpg',
-      'https://www.geovistory.org/carousel-6_bronzes-benin.jpg',
-      'https://www.geovistory.org/carousel-7_carte-amerique.jpg',
-      'https://www.geovistory.org/carousel-8_assemble-vizille.jpg',
-      'https://www.geovistory.org/carousel-9_acte-deces.jpg',
-      'https://www.geovistory.org/carousel-10_guerre-russo-japonaise.jpg',
-      'https://www.geovistory.org/carousel-11_congret-musique.jpg',
-      'https://www.geovistory.org/carousel-12_toronto.jpg',
-    ]}
+    images={[image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12]}
   ></geov-carousel>,
 );
---
+++ packages/design-system-web/src/components/geov-carousel/images/index.js
@@ -0,0 +1,16 @@
+// /images/index.js
+
+import image1 from './carousel-1_maquette-lugdunum.jpg';
+import image2 from './carousel-2_tapisserie-bayeux.jpg';
+import image3 from './carousel-3_vitrail.jpg';
+import image4 from './carousel-4_astrolabe-arabe.jpg';
+import image5 from './carousel-5_quetzalcoatl.jpg';
+import image6 from './carousel-6_bronzes-benin.jpg';
+import image7 from './carousel-7_carte-amerique.jpg';
+import image8 from './carousel-8_assemble-vizille.jpg';
+import image9 from './carousel-9_acte-deces.jpg';
+import image10 from './carousel-10_guerre-russo-japonaise.jpg';
+import image11 from './carousel-11_congret-musique.jpg';
+import image12 from './carousel-12_toronto.jpg';
+
+export { image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12 };
