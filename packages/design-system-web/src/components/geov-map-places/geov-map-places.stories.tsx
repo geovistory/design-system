4add50c194ae7ae0dafe68a6ff1465060e6d3922
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-10-12 13:31:47 +0200

    add Examples

--- packages/design-system-web/src/components/geov-map-places/geov-map-places.stories.tsx
+++ packages/design-system-web/src/components/geov-map-places/geov-map-places.stories.tsx
@@ -15,8 +15,35 @@ export default {
   },
 };
 
-export const MapPlaces = await stencilWrapper(
+export const Basic = await stencilWrapper(
   <div style={{ height: '350px' }}>
     <geov-map-places sparqlEndpoint="https://sparql.geovistory.org/api_v1_project_84760"></geov-map-places>
+  </div>,
+);
+
+/**
+ * If a ProjectID is provided, the markers on the map will be linked to the project.
+ */
+export const WithProjectID = await stencilWrapper(
+  <div style={{ height: '350px' }}>
+    <geov-map-places sparqlEndpoint="https://sparql.geovistory.org/api_v1_project_84760" projectID={84760}></geov-map-places>
+  </div>,
+);
+
+/**
+ * If the requested number of places exceeds the limit, the map will display a warning message.
+ */
+export const LimitExeeded = await stencilWrapper(
+  <div style={{ height: '350px' }}>
+    <geov-map-places sparqlEndpoint="https://sparql.geovistory.org/api_v1_project_84760" limit={10}></geov-map-places>
+  </div>,
+);
+
+/**
+ * If there is no bounding box selected, the map will request all Places from the project.
+ */
+export const NoBoundingBox = await stencilWrapper(
+  <div style={{ height: '350px' }}>
+    <geov-map-places sparqlEndpoint="https://sparql.geovistory.org/api_v1_project_84760" queryBoundingBox={false}></geov-map-places>
   </div>,
 );
