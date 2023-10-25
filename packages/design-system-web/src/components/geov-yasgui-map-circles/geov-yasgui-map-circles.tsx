116e10de9f1f44ddece887e8e664ef8efa7245d6
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-10-25 18:14:59 +0200

    add failsaves and fallbacks

--- packages/design-system-web/src/components/geov-yasgui-map-circles/geov-yasgui-map-circles.tsx
+++ packages/design-system-web/src/components/geov-yasgui-map-circles/geov-yasgui-map-circles.tsx
@@ -17,12 +17,12 @@ function createGeoJSON(data: Parser.Binding[]) {
         },
         //set the properties to the .value of the ele
         properties: {
-          label: ele['label'].value,
-          radius: Math.round(parseFloat(ele['radius'].value) * 10) / 10,
-          number: parseInt(ele['number'].value),
+          label: ele['label']?.value,
+          radius: Math.round((parseFloat(ele['radius']?.value) || 0) * 10) / 10,
+          number: parseInt(ele['number']?.value),
           type: ele['type']?.value || 'none',
           typeindex: labelIndices.indexOf(ele['type']?.value || 'none'),
-          link: ele['link'].value,
+          link: ele['link']?.value,
         },
       };
     }),
@@ -45,12 +45,12 @@ export class GeovYasguiMapCircles {
 
   @Prop() data: Parser.Binding[] = [
     {
-      radius: { value: '80.2345', type: 'literal' },
-      number: { value: '0', type: 'literal' },
-      type: { value: 'default', type: 'literal' },
+      radiuss: { value: '80.2345', type: 'literal' },
+      numbers: { value: '0', type: 'literal' },
+      types: { value: 'default', type: 'literal' },
       long: { value: '0', type: 'literal' },
       lat: { value: '0', type: 'literal' },
-      label: { value: 'default', type: 'literal' },
+      labels: { value: 'default', type: 'literal' },
       link: { value: 'default', type: 'literal' },
     },
   ];
@@ -74,113 +74,126 @@ export class GeovYasguiMapCircles {
         card.style.setProperty('display', 'block');
         card.querySelector('ion-card-title').innerHTML = `Unable to render ${invalidPoints.length} results`;
         card.querySelector('ion-card-content').innerHTML = `<p>not all of the results have longitude and latitude-coordinates:</p><ul><li>${invalidList}</li></ul>`;
-      }
-
-      // Load MapLibre script
-      const MapLibre = await importMapLibre();
-      const map = new MapLibre.Map({
-        container: this.el,
-        style: {
-          version: 8,
-          sources: {
-            osm: {
-              type: 'raster',
-              tiles: [
-                'https://a.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
-                'https://b.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
-                'https://c.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
-              ],
-              tileSize: 256,
-              attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-              maxzoom: 19,
+      } else {
+        // Load MapLibre script
+        const MapLibre = await importMapLibre();
+        const map = new MapLibre.Map({
+          container: this.el,
+          style: {
+            version: 8,
+            sources: {
+              osm: {
+                type: 'raster',
+                tiles: [
+                  'https://a.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
+                  'https://b.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
+                  'https://c.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{ratio}.png',
+                ],
+                tileSize: 256,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                maxzoom: 19,
+              },
             },
+            layers: [
+              {
+                id: 'osm',
+                type: 'raster',
+                source: 'osm', // This must match the source key above
+              },
+            ],
+            glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
           },
-          layers: [
-            {
-              id: 'osm',
-              type: 'raster',
-              source: 'osm', // This must match the source key above
-            },
-          ],
-          glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
-        },
-        zoom: 6,
-      });
-
-      /*
-       * move the map to the bounds of the data
-       */
-      let minLng = Number.MAX_VALUE;
-      let maxLng = Number.MIN_VALUE;
-      let minLat = Number.MAX_VALUE;
-      let maxLat = Number.MIN_VALUE;
-
-      // Find the bounds of the data
-      this.data.forEach(d => {
-        const lng = parseFloat(d['long'].value);
-        const lat = parseFloat(d['lat'].value);
-        minLng = Math.min(minLng, lng);
-        maxLng = Math.max(maxLng, lng);
-        minLat = Math.min(minLat, lat);
-        maxLat = Math.max(maxLat, lat);
-      });
-
-      const bounds: LngLatBoundsLike = [
-        [minLng, minLat],
-        [maxLng, maxLat],
-      ];
-
-      map.fitBounds(bounds, {
-        padding: 50, // Optional padding to provide some space around the bounding box
-      });
-
-      map.on('load', () => {
-        map.addSource('places', {
-          type: 'geojson',
-          data: createGeoJSON(this.data),
-        });
-        const maxRadius = Math.max(...this.data.map(d => parseInt(d['radius'].value)));
-        const colorSteps: Array<string | number | Array<string | number>> = ['step', ['get', 'typeindex']];
-        for (let i = 0; i < this.colorScale.length; i++) {
-          if (i > 0) colorSteps.push(i);
-          colorSteps.push(this.colorScale[i]);
-        }
-
-        map.addLayer({
-          id: 'circles',
-          type: 'circle',
-          source: 'places',
-          paint: {
-            'circle-color': colorSteps as DataDrivenPropertyValueSpecification<string>,
-            'circle-radius': ['interpolate', ['linear'], ['get', 'radius'], 0, 8, maxRadius, 60],
-            'circle-opacity': 0.8,
-          },
+          zoom: 6,
         });
 
-        //   Add popups to the markers
-        const handleMarkerClick = e => {
-          const coordinates = e.features[0].geometry.coordinates;
-          let html = `<ul>`;
-          e.features.forEach((feature: { properties: { link: string; label: string; number: string } }) => {
-            const props = feature.properties;
-            html += `<li>
-            <p><b><a href = "${props.link}" target="_blank">${props.label}</a></b>: ${props.number}</p>
-            </li>`;
+        /*
+         * move the map to the bounds of the data
+         */
+        let minLng = Number.MAX_VALUE;
+        let maxLng = Number.MIN_VALUE;
+        let minLat = Number.MAX_VALUE;
+        let maxLat = Number.MIN_VALUE;
+
+        // Find the bounds of the data
+        this.data.forEach(d => {
+          const lng = parseFloat(d['long'].value);
+          const lat = parseFloat(d['lat'].value);
+          minLng = Math.min(minLng, lng);
+          maxLng = Math.max(maxLng, lng);
+          minLat = Math.min(minLat, lat);
+          maxLat = Math.max(maxLat, lat);
+        });
+
+        const bounds: LngLatBoundsLike = [
+          [minLng, minLat],
+          [maxLng, maxLat],
+        ];
+
+        map.fitBounds(bounds, {
+          padding: 50, // Optional padding to provide some space around the bounding box
+        });
+
+        map.on('load', () => {
+          map.addSource('places', {
+            type: 'geojson',
+            data: createGeoJSON(this.data),
           });
-          html += `</ul>`;
-          new MapLibre.Popup().setLngLat(coordinates).setHTML(html).addTo(map);
-        };
+          const maxRadius = Math.max(...this.data.map(d => parseInt(d['radius']?.value) || 0)); // get the max radius (if any)
+          const colorSteps: Array<string | number | Array<string | number>> = ['step', ['get', 'typeindex']];
+          for (let i = 0; i < this.colorScale.length; i++) {
+            if (i > 0) colorSteps.push(i);
+            colorSteps.push(this.colorScale[i]);
+          }
 
-        // An on click event listener for the "circles" layer
-        map.on('click', 'circles', handleMarkerClick);
+          map.addLayer({
+            id: 'circles',
+            type: 'circle',
+            source: 'places',
+            paint: {
+              'circle-color': colorSteps as DataDrivenPropertyValueSpecification<string>,
+              'circle-radius': maxRadius ? ['interpolate', ['linear'], ['get', 'radius'], 0, 8, maxRadius, 60] : 8, // if there is no radius, use 8px
+              'circle-opacity': 0.8,
+            },
+          });
 
-        map.on('mouseenter', 'circles', () => {
-          map.getCanvas().style.cursor = 'pointer';
+          //   Add popups to the markers
+          const handleMarkerClick = e => {
+            const coordinates = e.features[0].geometry.coordinates;
+            let html = `<ul>`;
+            e.features.forEach((feature: { properties: { link: string; label: string; number: string } }) => {
+              const props = feature.properties;
+              // differentiate between a few cases: if theres a link, make it clickable, otherwise check if there also is missing a label
+              html += '<li>';
+              if (props?.link) {
+                html += `<p><b><a href = "${props.link}" target="_blank">${props?.label || 'no label'}</a></b>`;
+              } else if (props?.label) {
+                html += `<p><b>${props.label}</b>`;
+              } else {
+                html += `<p><b>no label</b>`;
+              }
+
+              if (props?.number) {
+                html += `: ${props.number}</p>`;
+              } else {
+                html += `</p>`;
+              }
+              html += '</li>';
+            });
+            html += `</ul>`;
+            new MapLibre.Popup().setLngLat(coordinates).setHTML(html).addTo(map);
+          };
+
+          // An on click event listener for the "circles" layer
+          map.on('click', 'circles', handleMarkerClick);
+
+          map.on('mouseenter', 'circles', () => {
+            map.getCanvas().style.cursor = 'pointer';
+          });
+          map.on('mouseleave', 'circles', () => {
+            map.getCanvas().style.cursor = '';
+          });
         });
-        map.on('mouseleave', 'circles', () => {
-          map.getCanvas().style.cursor = '';
-        });
-      });
+      }
     }
   }
 
