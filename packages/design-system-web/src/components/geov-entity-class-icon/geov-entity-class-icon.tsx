bc3062b8b5a4d5c6697dca0b1e0f3fe7a75072f3
Alexandre P <perraud.alexandre@gmail.com>
2023-10-30 14:50:37 +0100

    Add more icons. Little change in paginator

--- packages/design-system-web/src/components/geov-entity-class-icon/geov-entity-class-icon.tsx
+++ packages/design-system-web/src/components/geov-entity-class-icon/geov-entity-class-icon.tsx
@@ -5,21 +5,23 @@ import {
   briefcaseOutline,
   calendarClearOutline,
   calendarOutline,
+  chatbubbleEllipsesOutline,
   documentTextOutline,
+  earthOutline,
   ellipseOutline,
   heartHalfOutline,
   locationOutline,
-  peopleCircleOutline,
   peopleOutline,
-  personCircleOutline,
+  personOutline,
   skullOutline,
+  starOutline,
   transgenderOutline,
 } from 'ionicons/icons';
 
 const iconMap = {
-  'https://ontome.net/ontology/c21': personCircleOutline, // Person
+  'https://ontome.net/ontology/c21': personOutline, // Person
   'https://ontome.net/ontology/c363': locationOutline, // Geographical Place
-  'https://ontome.net/ontology/c68': peopleCircleOutline, // Group
+  'https://ontome.net/ontology/c68': peopleOutline, // Group
   'https://ontome.net/ontology/c523': boatOutline, // Ship Voyage
   'https://ontome.net/ontology/c50': calendarClearOutline, // Time Span
   'http://www.w3.org/2006/time#DateTimeDescription': calendarOutline, // Date Time Description
@@ -28,8 +30,18 @@ const iconMap = {
   'https://ontome.net/ontology/c61': balloonOutline, // Birth
   'https://ontome.net/ontology/c633': heartHalfOutline, // Relationship
   'https://ontome.net/ontology/c442': peopleOutline, // Membership
-  'https://ontome.net/ontology/c636': briefcaseOutline, // Occupation
   'https://ontome.net/ontology/c629': transgenderOutline, // Gender
+  'https://ontome.net/ontology/c697': briefcaseOutline, // Embodiment
+  'https://ontome.net/ontology/c698': briefcaseOutline, // Actors social role
+  'https://ontome.net/ontology/c213': briefcaseOutline, // Social Quality of an actor
+  'https://ontome.net/ontology/c444': briefcaseOutline, // Actors social quality
+  'https://ontome.net/ontology/c636': briefcaseOutline, // Occupation
+  'https://ontome.net/ontology/c637': briefcaseOutline, // Occupation (Temporal entity)
+  'https://ontome.net/ontology/c5': starOutline, // Event
+  'https://ontome.net/ontology/c535': starOutline, // Participation
+  'https://ontome.net/ontology/c933': documentTextOutline, // Annotation in Text
+  'https://ontome.net/ontology/c84': earthOutline, // Georeference
+  'https://ontome.net/ontology/c868': chatbubbleEllipsesOutline, // Person Apppellation in a Language
 };
 const defaultIcon = ellipseOutline;
 
