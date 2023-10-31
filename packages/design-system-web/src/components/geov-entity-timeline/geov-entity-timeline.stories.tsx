02a8f899e97d54c7982833893232f96f99f93bb4
Alexandre P <perraud.alexandre@gmail.com>
2023-10-31 10:06:28 +0100

    intermediate commit

---
+++ packages/design-system-web/src/components/geov-entity-timeline/geov-entity-timeline.stories.tsx
@@ -0,0 +1,17 @@
+import { h } from '@stencil/core';
+import { stencilWrapper } from '../../../.storybook/lib/stencilWrapper';
+import { docsTemlpate } from '../../../.storybook/templates/docsTemplate';
+import componentApi from './docs-component-api.md?raw';
+import overview from './docs-overview.md?raw';
+export default {
+  title: 'Data Components/Entity/Entity Timeline',
+  tags: ['autodocs'],
+  parameters: {
+    viewMode: 'docs',
+    docs: {
+      page: () => docsTemlpate(overview, componentApi),
+    },
+  },
+};
+
+export const Test = await stencilWrapper(<geov-timeline></geov-timeline>);
