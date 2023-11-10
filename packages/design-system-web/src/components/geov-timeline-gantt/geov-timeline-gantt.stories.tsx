0f5914730c286494c1b3f5801fc8e4e1a07a3735
Alexandre P <perraud.alexandre@gmail.com>
2023-11-10 13:56:10 +0100

    Add chartjs

--- packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.stories.tsx
+++ packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.stories.tsx
@@ -4,7 +4,7 @@ import { docsTemlpate } from '../../../.storybook/templates/docsTemplate';
 import componentApi from './docs-component-api.md?raw';
 import overview from './docs-overview.md?raw';
 export default {
-  title: 'Data Components/Entity/Entity Timeline',
+  title: 'Data Visualization Components/Timeline Gantt',
   tags: ['autodocs'],
   parameters: {
     viewMode: 'docs',
@@ -14,4 +14,4 @@ export default {
   },
 };
 
-export const Test = await stencilWrapper(<geov-timeline></geov-timeline>);
+export const Test = await stencilWrapper(<geov-timeline-gantt></geov-timeline-gantt>);
