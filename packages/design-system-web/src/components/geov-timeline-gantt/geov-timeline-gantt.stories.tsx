e7b9829dd48819820f092c510e5c75eaa449cd25
Alexandre P <perraud.alexandre@gmail.com>
2023-12-04 14:46:11 +0100

    Timeline first prototype

--- packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.stories.tsx
+++ packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.stories.tsx
@@ -23,37 +23,56 @@ type timespanData = {
 };
 
 const ts1: timespanData = {
-  entityLabel: 'Example Entity 1',
-  entityClassLabel: 'Example Class 1',
+  entityLabel: 'Birth of Johann Kepler',
+  entityClassLabel: 'Birth',
   entityUri: 'https://example.com/entity',
-  startDate: '2022-01-01',
-  endDate: '2022-01-01',
+  startDate: '1571-12-27',
+  endDate: '1571-12-27',
 };
 
 const ts2: timespanData = {
-  entityLabel: 'Example Entity 2',
-  entityClassLabel: 'Example Class 2',
+  entityLabel: 'Death of Johann Kepler',
+  entityClassLabel: 'Death',
   entityUri: 'https://example.com/entity',
-  startDate: '2022-05-01',
-  endDate: '2022-05-03',
+  startDate: '1630-11-15',
+  endDate: '1630-11-15',
 };
 
 const ts3: timespanData = {
-  entityLabel: 'Example Entity 3',
-  entityClassLabel: 'Example Class 3',
+  entityLabel: 'One moment in his life',
+  entityClassLabel: 'Live',
   entityUri: 'https://example.com/entity',
-  startDate: '2022-08-01',
-  endDate: '2022-12-31',
+  startDate: '1600-01-01',
+  endDate: '1600-12-31',
 };
 
 const ts4: timespanData = {
-  entityLabel: 'Example Entity 4',
-  entityClassLabel: 'Example Class 4',
-  entityUri: 'https://www.google.com',
-  startDate: '2023-01-01',
-  endDate: '2023-12-31',
+  entityLabel: 'Another moment in his life',
+  entityClassLabel: 'Live',
+  entityUri: 'https://example.com/entity',
+  startDate: '1610-01-01',
+  endDate: '1620-12-31',
 };
 
-const dataTimespansTest = [ts1, ts2, ts3, ts4];
+const ts5: timespanData = {
+  entityLabel: 'Another moment in his life 2',
+  entityClassLabel: 'Live',
+  entityUri: 'https://example.com/entity',
+  startDate: '1590-01-01',
+  endDate: '1595-12-31',
+};
 
-export const Test = await stencilWrapper(<geov-timeline-gantt data={dataTimespansTest}></geov-timeline-gantt>);
+const dataTimespansTest = [ts1, ts2, ts3, ts4, ts5];
+export const Base = await stencilWrapper(<geov-timeline-gantt data={dataTimespansTest}></geov-timeline-gantt>);
+
+/**
+ * Use lineHeight to determine the thickness, in pixels, of a line of data, and barPercentage to determine the percentage thickness of the bar in relation to the thickness of its line
+ */
+export const lineHeightAndBarPercentage = await stencilWrapper(<geov-timeline-gantt data={dataTimespansTest} lineHeight={50} barPercentage={0.3}></geov-timeline-gantt>);
+
+/**
+ * Use backgroundColor to choose the colour of the bar and borderColor to choose the colour of the border, if borderWidth > 0.
+ */
+export const colors = await stencilWrapper(
+  <geov-timeline-gantt data={dataTimespansTest} backgroundColor={[0, 0, 255, 1]} borderWidth={2} borderColor={[255, 0, 0, 1]}></geov-timeline-gantt>,
+);
--- packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.tsx
+++ packages/design-system-web/src/components/geov-timeline-gantt/geov-timeline-gantt.tsx
@@ -2,6 +2,7 @@ import { Component, h, Host, Prop } from '@stencil/core';
 import Chart from 'chart.js/auto';
 import 'chartjs-adapter-date-fns';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
 type timespanData = {
   entityLabel: string;
@@ -21,38 +22,102 @@ type timespanData = {
 })
 export class GeovTimelineGantt {
   el: HTMLCanvasElement;
+  axisCanvas: HTMLCanvasElement;
 
   @Prop() data: timespanData[];
 
   @Prop() lineHeight: number = 20;
 
+  @Prop() barPercentage: number = 1;
+
+  @Prop() backgroundColor: number[] = [122, 89, 199, 1];
+
+  @Prop() borderColor: number[] = [94, 62, 170, 1];
+
+  @Prop() borderWidth: number = 0;
+
+  @Prop() minBarLength: number = 40;
+
+  // Find the earliest date in the data
+  getEarliestDate(data: timespanData[]) {
+    // Initialization
+    let earliestDate = new Date(data[0].startDate);
+
+    for (let i = 1; i < data.length; i++) {
+      const currentDate = new Date(data[i].startDate);
+      if (currentDate < earliestDate) {
+        earliestDate = currentDate;
+      }
+    }
+    return earliestDate;
+  }
+
+  // Find the earliest date in the data
+  getLatestDate(data: timespanData[]) {
+    // Initialization
+    let latestDate = new Date(data[0].endDate);
+
+    for (let i = 1; i < data.length; i++) {
+      const currentDate = new Date(data[i].endDate);
+      if (currentDate > latestDate) {
+        latestDate = currentDate;
+      }
+    }
+    return latestDate;
+  }
+
+  // Compare two startDate
+  compareStartDate(tsA: timespanData, tsB: timespanData) {
+    const dateA = new Date(tsA.startDate).getTime();
+    const dateB = new Date(tsB.startDate).getTime();
+    return dateA - dateB;
+  }
+
+  // Sort by startDate
+  sortByStartDate(data: timespanData[]) {
+    data.sort(this.compareStartDate);
+  }
+
   componentDidLoad() {
-    const ctx = this.el.getContext('2d');
-    const chart = new Chart(ctx, {
+    // Sort by startDate
+    this.sortByStartDate(this.data);
+    const ctxMain = this.el.getContext('2d');
+    const chartMain = new Chart(ctxMain, {
       type: 'bar',
       data: {
-        //labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [
           {
             label: 'Timeline',
             data: this.data.map(item => ({ x: [item.startDate, item.endDate], y: item.entityLabel, name: item.entityClassLabel, entityUri: item.entityUri })),
-            backgroundColor: ['rgba(122, 89, 199)'],
-            borderColor: ['rgba(94, 62, 170)'],
-            barPercentage: 1,
+            backgroundColor: ['rgba(' + this.backgroundColor.join(',') + ')'],
+            borderColor: ['rgba(' + this.borderColor.join(',') + ')'],
+            borderWidth: this.borderWidth,
+            barPercentage: this.barPercentage,
+            minBarLength: this.minBarLength,
             borderSkipped: false,
             borderRadius: 10,
-            minBarLength: 40,
           },
         ],
       },
       options: {
         plugins: {
+          zoom: {
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              mode: 'x',
+            },
+          },
+          legend: {
+            display: false,
+          },
           tooltip: {
-            enabled: false,
+            enabled: true,
           },
           datalabels: {
             formatter: context => {
-              const label = context.y;
+              const label = context.name + ', ' + context.y;
               return `${label}`;
             },
             color: 'white',
@@ -61,40 +126,59 @@ export class GeovTimelineGantt {
         indexAxis: 'y',
         scales: {
           x: {
-            min: '2022',
+            min: (this.getEarliestDate(this.data).getFullYear() - 10).toString(),
+            max: (this.getLatestDate(this.data).getFullYear() + 10).toString(),
             type: 'time',
             time: {
               unit: 'year',
             },
+            ticks: {
+              stepSize: 2,
+            },
           },
           y: {
+            ticks: {
+              display: false,
+            },
             beginAtZero: true,
           },
         },
       },
-      plugins: [ChartDataLabels],
+      plugins: [ChartDataLabels, zoomPlugin],
     });
 
-    // Ajoutez un gestionnaire d'événements de clic pour détecter les clics sur les barres du graphique
+    // Click event handler to detect clicks on graph bars
     this.el.addEventListener('click', event => {
-      const activeElements = chart.getElementsAtEventForMode(event, 'nearest', { intersect: true }, true);
+      const activeElements = chartMain.getElementsAtEventForMode(event, 'nearest', { intersect: true }, true);
       if (activeElements.length > 0) {
         const clickedElement = activeElements[0];
         const datasetIndex = clickedElement.datasetIndex;
         const index = clickedElement.index;
-        const data = chart.data.datasets[datasetIndex].data[index];
+        const data = chartMain.data.datasets[datasetIndex].data[index];
 
-        // Récupérez l'URL associée à l'élément cliqué et redirigez la page
+        // Retrieve the URL associated with the clicked element and redirect the page
         const entityUri = data.entityUri;
         window.open(entityUri, '_blank');
       }
     });
   }
 
+  resetZoom() {
+    const chartMain = Chart.getChart(this.el);
+    if (chartMain) {
+      chartMain.resetZoom();
+    }
+  }
+
   render() {
     return (
       <Host>
-        <canvas id="myChart" height={this.data.length * this.lineHeight} ref={element => (this.el = element)}></canvas>
+        <ion-button onClick={() => this.resetZoom()}>Reset zoom</ion-button>
+        <div class="scrollBox">
+          <div class="scrollBoxBody">
+            <canvas id="chartMain" height={this.data.length * this.lineHeight} ref={element => (this.el = element)}></canvas>
+          </div>
+        </div>
       </Host>
     );
   }
