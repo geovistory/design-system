b6fa5a97dc7d155c102c18b95e88bbaed58b2c82
joschne <jonas.schneider@balcab.ch>
2024-02-06 14:30:32 +0100

    add component for map popup

---
+++ packages/design-system-web/src/components/geov-map-circles-popup/geov-map-circles-popup.stories.tsx
@@ -0,0 +1,36 @@
+import { h } from '@stencil/core';
+import { stencilWrapper } from '../../../.storybook/lib/stencilWrapper';
+import { docsTemlpate } from '../../../.storybook/templates/docsTemplate';
+import componentApi from './docs-component-api.md?raw';
+import overview from './docs-overview.md?raw';
+export default {
+  title: 'Data Visualization Components/Map Circles/Popup',
+  tags: ['autodocs'],
+  parameters: {
+    viewMode: 'docs',
+    docs: {
+      page: () => docsTemlpate(overview, componentApi),
+    },
+  },
+};
+
+export const Default = await stencilWrapper(
+  <geov-map-circles-popup
+    style={{ height: '200px' }}
+    items={[
+      { label: 'Label 1', items: [{ label: 'Child 1.1' }, { label: 'Child 1.2' }] },
+      {
+        label: 'Label 2',
+        suffix: '2',
+        url: 'http://foo.2.bar',
+        items: [
+          { label: 'Child 2.1', url: 'http://foo.2.1.bar' },
+          { label: 'Child 2.2', url: 'http://foo.2.2.bar' },
+          { label: 'Child 2.2', url: 'http://foo.2.2.bar' },
+          { label: 'Child 2.2', url: 'http://foo.2.2.bar' },
+          { label: 'Child 2.2', url: 'http://foo.2.2.bar' },
+        ],
+      },
+    ]}
+  ></geov-map-circles-popup>,
+);
---
+++ packages/design-system-web/src/components/geov-map-circles-popup/geov-map-circles-popup.tsx
@@ -0,0 +1,52 @@
+import { Component, Host, Prop, h } from '@stencil/core';
+
+export interface PopupSubitem {
+  label: string;
+  url?: string;
+}
+export interface PopupItem {
+  label: string;
+  url?: string;
+  suffix?: string;
+  items?: PopupSubitem[];
+}
+/**
+ * This component is used as content of the popup appearing, when user
+ * clicks on a circle on the map circles component.
+ */
+@Component({
+  tag: 'geov-map-circles-popup',
+  styleUrl: 'geov-map-circles-popup.css',
+  shadow: true,
+})
+export class GeovMapCirclesPopup {
+  @Prop() items: PopupItem[] = [];
+  render() {
+    return (
+      <Host>
+        <ul>
+          {this.items.map(i => (
+            <li>
+              {i.url ? <a href={i.url}>{i.label}</a> : i.label} {i.suffix && <small>({i.suffix})</small>}
+              {i?.items?.length > 0 && (
+                <ul>
+                  {i.items.map(subitem => (
+                    <li>
+                      {subitem.url ? (
+                        <a class="subitem" href={subitem.url}>
+                          {subitem.label}
+                        </a>
+                      ) : (
+                        <span class="subitem">{subitem.label}</span>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </Host>
+    );
+  }
+}
